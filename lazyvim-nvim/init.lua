7f02a4ad66434e675d3e1c7310e5eea1cc93efd4
dillontkh <e0958762@u.nus.edu>
2024-03-11 17:39:05 +0800

    Complete harpoon migration

--- lazyvim-nvim/init.lua
+++ lazyvim-nvim/init.lua
@@ -7,5 +7,4 @@ else
   -- Ordinary NeoVim
   -- bootstrap lazy.nvim, LazyVim and your plugins
   require("config.lazy")
-  require("config.harpoon")
 end
--- lazyvim-nvim/lua/plugins/harpoon.lua
+++ lazyvim-nvim/lua/plugins/harpoon.lua
@@ -1,10 +1,10 @@
 return {
   "ThePrimeagen/harpoon",
   branch = "harpoon2",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
-  },
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("harpoon"):setup({})
+  end,
   keys = function()
     local harpoon = require("harpoon")
     return {
@@ -14,6 +14,55 @@ return {
           harpoon:list():append()
         end,
         desc = "Add buffer to harpoon",
+      },
+      {
+        "<leader>h",
+        function()
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Open harpoon menu",
+      },
+      {
+        "<leader>1",
+        function()
+          harpoon:list():select(1)
+        end,
+        desc = "Quick swap to harpoon 1 ",
+      },
+      {
+        "<leader>2",
+        function()
+          harpoon:list():select(2)
+        end,
+        desc = "Quick swap to harpoon 2 ",
+      },
+      {
+        "<leader>3",
+        function()
+          harpoon:list():select(3)
+        end,
+        desc = "Quick swap to harpoon 3 ",
+      },
+      {
+        "<leader>4",
+        function()
+          harpoon:list():select(4)
+        end,
+        desc = "Quick swap to harpoon 4 ",
+      },
+      {
+        "<C-[>",
+        function()
+          harpoon:list():prev()
+        end,
+        desc = "Switch to previous harpoon window",
+      },
+      {
+        "<C-]>",
+        function()
+          harpoon:list():next()
+        end,
+        desc = "Switch to next harpoon window",
       },
     }
   end,
